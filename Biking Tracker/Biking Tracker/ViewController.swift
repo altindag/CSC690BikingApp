7e0776bae1712a3bf3ca1a1cc5124a58943ae1af
acavit <cavitaltindag@gmail.com>
2018-12-09 05:21:12 -0800

    added start and stop point and refresh map

--- Biking Tracker/Biking Tracker/ViewController.swift
+++ Biking Tracker/Biking Tracker/ViewController.swift
@@ -13,7 +13,7 @@ class BikeAnnotation: NSObject, MKAnnotation{
     var coordinate: CLLocationCoordinate2D
     var title: String?
     var subtitle: String?
-    
+  
     init(coordinate: CLLocationCoordinate2D, title: String?, subtitle:String?){
         self.coordinate = coordinate
         self.title = title
@@ -22,49 +22,75 @@ class BikeAnnotation: NSObject, MKAnnotation{
     }
     
     var region: MKCoordinateRegion{
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let span = MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002)
         return MKCoordinateRegion(center: coordinate, span: span)
     }
 }
+
+
 class ViewController: UIViewController, CLLocationManagerDelegate ,MKMapViewDelegate {
 var timer = Timer()
 var totalTime : Int = 0
 var running : Bool = false;
+      var annotationStartPoint : Int  = -1
     @IBOutlet weak var startTracking: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var clearMap: UIButton!
     @IBOutlet weak var durationHolder: UILabel!
     @IBOutlet weak var startTimeHolder: UILabel!
- var locationManager = CLLocationManager()
+    @IBOutlet weak var stopTracking: UIButton!
+    var locationManager = CLLocationManager()
     var road: [MKCircle] = []
+    var roadAnnotations: [BikeAnnotation] = []
 
     
     @IBAction func startTrackingclick(_ sender: UIButton) {
-        
+        self.annotationStartPoint = self.road.count
+        print("initial array :\(self.annotationStartPoint)")
         if (running == false) {
             totalTime = 0
             durationHolder.text = ""
             running = true;
-            startTracking.setImage(UIImage(named: "stopicon.png"), for: .normal)
+            //startTracking.setImage(UIImage(named: "stopicon.png"), for: .normal)
            
             print("start biking")
             startTimeHolder.text = DateFormatter.localizedString(from: Date(), dateStyle: .none, timeStyle: .short)
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.tick) , userInfo: nil, repeats: true)
-            mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
-            let instanceCoordinate = CLLocationCoordinate2D(latitude: 42.361, longitude: -71.0942)
-            let instanceAnnotation = BikeAnnotation(coordinate: instanceCoordinate, title: "start location", subtitle: "we started here")
-            locationManager.startUpdatingLocation()
             
-        }else{
-            locationManager.stopUpdatingLocation()
-            timer.invalidate()
-            running = false
-            startTracking.setImage(UIImage(named: "starticon.png"), for: .normal)
-
+            locationManager.startUpdatingLocation()
+            addPoint((locationManager.location?.coordinate)!, "start")
+          
+            
             
         }
     }
 
+    func addPoint(_ coor:CLLocationCoordinate2D, _ title : String){
+        self.mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
+        let instanceCoordinate = CLLocationCoordinate2D(latitude: coor.latitude, longitude: coor.longitude)
+        let instanceAnnotation = BikeAnnotation(coordinate: instanceCoordinate, title: title, subtitle: "")
+        roadAnnotations.append(instanceAnnotation)
+        
+        self.mapView.addAnnotation(instanceAnnotation)
+    }
+    
+    @IBAction func stopTrackingClick(_ sender: UIButton) {
+        if (running == true) {
+            locationManager.stopUpdatingLocation()
+            timer.invalidate()
+            running = false
+            //startTracking.setImage(UIImage(named: "starticon.png"), for: .normal)
+            addPoint((self.locationManager.location?.coordinate)!,"stop")
+        }
+        
+    }
+    @IBAction func clearMapClicked(_ sender: Any) {
+        
+        self.mapView.removeOverlays(self.road)
+        self.road = []
+        
+    }
     func updateTime() {
        
          totalTime += 1
@@ -84,15 +110,13 @@ var running : Bool = false;
         updateTime()
     }
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]){
-        
+        if (running == true){
         let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.002, longitudeDelta: 0.002) )
         
-        self.mapView.setRegion(region, animated: false)
-        print(locations[0].coordinate.latitude)
-        print(locations.count)
-        
-        self.road.append(MKCircle(center: region.center , radius: 2))
-        self.mapView.addOverlays(self.road, level: .aboveLabels)
+            self.mapView.setRegion(region, animated: false)
+            self.road.append(MKCircle(center: region.center , radius: 2))
+            self.mapView.addOverlays(self.road, level: .aboveLabels)
+        }
         
     }
     
@@ -102,17 +126,16 @@ var running : Bool = false;
         
         circleRenderer.strokeColor = UIColor.blue
         circleRenderer.lineWidth = 1.0
-        print("overlay")
         return circleRenderer
         
     }
     override func viewDidLoad() {
        
         super.viewDidLoad()
-        super.viewDidLoad()
+
         mapView.delegate = self
         mapView.showsUserLocation = true
-        self.mapView.setUserTrackingMode(.followWithHeading, animated: false)
+        
         if CLLocationManager.locationServicesEnabled() == true {
             if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .denied || CLLocationManager.authorizationStatus() == .notDetermined{
                 
@@ -121,6 +144,7 @@ var running : Bool = false;
             }
             locationManager.desiredAccuracy = 1.0
             locationManager.delegate = self
+            
             
             
             
